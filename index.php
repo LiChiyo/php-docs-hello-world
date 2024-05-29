238bbb84fe0b55190b677eef0df5380a2b2e1620
LiChiyo <782339214@qq.com>
2024-05-29 13:39:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 
+function goHome() {
+  header('Location: https://my-first-app-11.azurewebsites.net/home.php');
+}
+
 echo "Hello World!"; ?>
-<input type="button" value="ログイン" onclick=""/>
+<input type="button" value="ログイン" onclick="goHome()"/>
